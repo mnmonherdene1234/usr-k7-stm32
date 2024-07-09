d7e1634a32124fd6b9acb18878954c8fb0619c8e
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2024-07-09 12:08:19 +0800

    working v1

--- Core/Inc/USRK7.h
+++ Core/Inc/USRK7.h
@@ -12,17 +12,29 @@
 #include <string.h>
 #include "stm32f1xx_hal.h"
 
-#define USRK7_BUFFER_SIZE 64
-#define USRK7_COMMAND_SIZE 64
+#define USRK7_BUFFER_SIZE 512
+#define USRK7_COMMAND_SIZE 512
 
-#define USRK7_SERVER_IP "185.80.128.201"
-#define USRK7_SERVER_PORT 3911
-#define USRK7_UDP_PORT 80
+#define USRK7_IP "192.168.1.8"
+#define USRK7_SUBMASK "192.168.1.8"
+#define USRK7_GATEWAY "192.168.1.1"
+#define USRK7_DNS "8.8.8.8"
+
+#define USRK7_SERVER_IP "192.168.1.10"
+#define USRK7_SERVER_PORT 3399
+
+typedef enum {
+	USRK7_OK = 0, USRK7_ERROR = 1,
+} USRK7_Status;
 
 extern UART_HandleTypeDef *USRK7_UART;
 extern char USRK7_BUFFER[USRK7_BUFFER_SIZE];
 extern char USRK7_COMMAND[USRK7_COMMAND_SIZE];
 
+HAL_StatusTypeDef USRK7_transmit(const char *command, int timeout);
+USRK7_Status USRK7_receive(int attempt, int timeout);
+USRK7_Status USRK7_transmit_receive(const char *command, int tTimeout,
+		int rTimeout, int attempt);
 int USRK7_init(UART_HandleTypeDef *huart);
 
 #endif /* INC_USRK7_H_ */
--- Core/Src/USRK7.c
+++ Core/Src/USRK7.c
@@ -4,43 +4,100 @@ UART_HandleTypeDef *USRK7_UART;
 char USRK7_BUFFER[USRK7_BUFFER_SIZE];
 char USRK7_COMMAND[USRK7_COMMAND_SIZE];
 
-static void USRK7_transmit(const char *command, int timeout) {
-	HAL_UART_Transmit(USRK7_UART, (uint8_t*) command, strlen(command), timeout);
-}
-
-static void USRK7_receive(int timeout) {
-	sprintf(USRK7_BUFFER, "");
-	HAL_UART_Receive(USRK7_UART, (uint8_t*) USRK7_BUFFER, USRK7_BUFFER_SIZE,
+HAL_StatusTypeDef USRK7_transmit(const char *command, int timeout) {
+	return HAL_UART_Transmit(USRK7_UART, (uint8_t*) command, strlen(command),
 			timeout);
 }
 
-static void USRK7_transmit_receive(const char *command, int tTimeout,
-		int rTimeout) {
-	USRK7_transmit(command, tTimeout);
-	USRK7_receive(rTimeout);
+USRK7_Status USRK7_receive(int attempt, int timeout) {
+	memset(USRK7_BUFFER, 0, USRK7_BUFFER_SIZE);
+
+	for (int i = 0; i < attempt; i++) {
+		HAL_UART_Receive(USRK7_UART, (uint8_t*) USRK7_BUFFER, USRK7_BUFFER_SIZE,
+				timeout);
+
+		if (strlen(USRK7_BUFFER) >= 1) {
+			return USRK7_OK;
+		}
+	}
+
+	return USRK7_ERROR;
+}
+
+USRK7_Status USRK7_transmit_receive(const char *command, int tTimeout,
+		int rTimeout, int attempt) {
+	HAL_StatusTypeDef transmitResult = USRK7_transmit(command, tTimeout);
+
+	if (transmitResult != HAL_OK) {
+		return USRK7_ERROR;
+	}
+
+	return USRK7_receive(attempt, rTimeout);
 }
 
 int USRK7_init(UART_HandleTypeDef *uart) {
 	USRK7_UART = uart;
 
-	sprintf(USRK7_COMMAND, "+++");
-	USRK7_transmit(USRK7_COMMAND, 3000);
-	USRK7_receive(200);
+	USRK7_Status status = USRK7_OK;
 
-	if (strstr(USRK7_BUFFER, "a")) {
-		HAL_Delay(200);
-		sprintf(USRK7_COMMAND, "a");
-		USRK7_transmit(USRK7_COMMAND, 1000);
-
-		// Step 4: Wait for '+ok' response
-		USRK7_receive(3000);
-		if (strstr(USRK7_BUFFER, "+ok")) {
-			sprintf(USRK7_COMMAND, "AT+VER\r\n");
-			USRK7_transmit_receive(USRK7_COMMAND, 200, 200);
-
-			return 0; // Initialization successful
-		}
+	sprintf(USRK7_COMMAND, "AT+ENTM\r\n");
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
 	}
 
-	return -1; // Initialization failed
+	sprintf(USRK7_COMMAND, "+++");
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
+	}
+
+	if (!strstr(USRK7_BUFFER, "a")) {
+		return USRK7_ERROR;
+	}
+
+	HAL_Delay(130);
+	sprintf(USRK7_COMMAND, "a");
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
+	}
+
+	if (!strstr(USRK7_BUFFER, "+ok")) {
+		return USRK7_ERROR;
+	}
+
+	sprintf(USRK7_COMMAND, "AT+WANN=static,%s,%s,%s\r\n", USRK7_IP,
+	USRK7_SUBMASK, USRK7_GATEWAY);
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
+	}
+
+	sprintf(USRK7_COMMAND, "AT+SOCKA1=UDPC,%s,%d\r\n", USRK7_SERVER_IP,
+	USRK7_SERVER_PORT);
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
+	}
+
+	sprintf(USRK7_COMMAND, "AT+DNS=%s\r\n", USRK7_DNS);
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
+	}
+
+	sprintf(USRK7_COMMAND, "AT+Z\r\n");
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
+	}
+
+	sprintf(USRK7_COMMAND, "AT+ENTM\r\n");
+	status = USRK7_transmit_receive(USRK7_COMMAND, 1000, 150, 10);
+	if (status != USRK7_OK) {
+		return status;
+	}
+
+	return 0;
 }
